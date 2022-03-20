9f386ea7da5f991ef77579d946d0eb109aa66fde
Elias Strauß <eliasstrauss@Eliass-MBP-3.fritz.box>
2022-03-20 10:31:33 +0100

    auto

--- terraform/startup.sh
+++ terraform/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 yum update -y
-yum install pip
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
 yum install git
 git clone https://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@github.com/Elias-Strauss/fast-flat-bot.git
+ ./.local/bin/pip install requirements.txt
