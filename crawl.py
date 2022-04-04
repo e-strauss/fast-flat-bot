b5431ca015e2b8f481a72a6cc4c4711cecb8d7ef
Elias Strauß <eliasstrauss@Eliass-MBP-3.fritz.box>
2022-04-04 23:51:21 +0200

    auto

--- crawl.py
+++ crawl.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 
 url = 'https://www.immobilienscout24.de/Suche/de/berlin/berlin/wohnung-mieten?numberofrooms=-3.5&price=-950.0&exclusioncriteria=swapflat&pricetype=calculatedtotalrent&geocodes=1100000001,1100000003,110000000201&sorting=2'
 
-#driver = webdriver.Firefox()
-driver = webdriver.Safari()
+driver = webdriver.Firefox()
 driver.get(url)
 print(driver)
